d355d26fbfb6daa3580086a84fde2d708c853101
gatosangria <gthlbrc@outlook.com>
2022-10-26 22:20:55 -0300

    AULA 8, JS

---
+++ Aula08/Aula08.js
@@ -0,0 +1,24 @@
+console.log("Olá mundo!!!S2")
+
+/* console.log é um comando que vai exibir informações
+no console do navegador */
+
+var variavel1 = 2;
+// var tem o tipo de escopo global
+let variavel2 = "Olá";
+// let tem o tipo de escopo local
+const variavel3 = 3 + 3;
+// const é de escopo global e não pode ser reescrita
+
+console.log("A variável1 é igual a", variavel1)
+
+variavel1 = 4
+
+console.log("Agora a variavel1 é igual a", variavel1)
+
+console.log("A variavel2 é ", variavel2)
+
+variavel2 = 6
+
+console.log("Agora a variavel2 vale", variavel2)
+
